240463a779b7c6bc3e3726e913dc692e63ce3255
okothhillary <hobboltport@gmail.com>
2024-06-18 19:36:32 +0300

    correct file path

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
 // Get the current year
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = currentYear;
+document.getElementById('currentyear').innerHTML = currentYear;
 
 // Get the last modified date
 const lastModified = document.lastModified;
-document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+document.getElementById('lastModified').innerHTML = `Last Modified: ${lastModified}`;

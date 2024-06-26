95ba563e27a1c806a4ffbdc421cb63231bd502ee
okothhillary <hobboltport@gmail.com>
2024-06-26 09:00:44 +0300

    hamburger menu

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -6,15 +6,15 @@ document.getElementById('currentyear').innerHTML = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').innerHTML = `Last Modified: ${lastModified}`;
 
-
+//hamburger menu
 const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('nav-menu');
 
     hamburger.addEventListener('click', function() {
         navMenu.classList.toggle('active');
-        if (hamburger.textContent === '\u2630') { // '☰' symbol
+        if (hamburger.textContent === '\u2630') { 
             hamburger.textContent = 'X';
         } else {
-            hamburger.textContent = '\u2630'; // '☰' symbol
+            hamburger.textContent = '\u2630'; 
         }
     });   
